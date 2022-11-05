2691aaa0fcabe8507a77c1d493c45ed44136af00
MohammedAfthab18 <85329977+MohammedAfthab18@users.noreply.github.com>
2022-11-05 20:46:40 +0530

    Update Webscrapping.r

--- Webscrapping.r
+++ Webscrapping.r
@@ -1,1 +1,13 @@
-
+#install packages
+library(robotstxt)
+library(rvest)
+Path = paths_allowed("https://www.imdb.com/chart/top/")
+url<- "https://www.imdb.com/chart/top"
+my_html<- read_html(url)
+my_tables <- html_nodes(my_html,"table")
+movie_table <- html_table(my_tables)[[1]]
+View(movie_table)
+movies <- movie_table[,2:3]
+View(movies)
+str(movies)
+write.csv(movies,"My_movies.csv")
